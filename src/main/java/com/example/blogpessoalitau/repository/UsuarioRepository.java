dea4d35ce41b312a9428ecdd4b899be1e27b8e67
pessoamarcio <pessoa-marcio@hotmail.com>
2023-02-22 20:38:19 -0300

    first commit

--- src/main/java/com/example/blogpessoalitau/repository/UsuarioRepository.java
+++ src/main/java/com/example/blogpessoalitau/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.example.blogpessoalitau.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,7 @@ import com.example.blogpessoalitau.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-	public Optional<Usuario> findByUsuario(@Param("usuario") String usuario);
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public List <Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
